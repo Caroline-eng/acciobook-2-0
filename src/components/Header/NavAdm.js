afb5b2797d97699358d326b9bc004a528ba48a63
Caroline-eng <115654361+Caroline-eng@users.noreply.github.com>
2023-05-15 21:20:33 -0300

    [fix] - + ajustes

--- src/components/Header/NavAdm.js
+++ src/components/Header/NavAdm.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Link } from "react-router-dom";
 
 import Modal from "react-bootstrap/Modal";
